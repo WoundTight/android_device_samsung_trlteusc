8ecd36de6c86d51055ff5f6dc38a81bcc51a5830
WoundTight <dirtmodified@gmail.com>
2015-11-17 11:08:50 -0600

    Create US Cellular cm-12.1 - fix data issues

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -18,7 +18,7 @@
 TARGET_OTA_ASSERT_DEVICE := trlteusc,SM-N910R4
 
 # Kernel
-TARGET_KERNEL_VARIANT_CONFIG := apq8084_sec_trlte_usc_defconfig
+TARGET_KERNEL_VARIANT_CONFIG := apq8084_sec_trlte_spr_defconfig
 
 # Init
 TARGET_INIT_VENDOR_LIB := libinit_msm
