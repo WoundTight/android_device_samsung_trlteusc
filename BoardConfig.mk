0152a85f6efb39023cb387d8ba6143190e59840e
WoundTight <dirtmodified@gmail.com>
2015-12-25 21:55:38 -0600

    next

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -25,8 +25,5 @@ TARGET_INIT_VENDOR_LIB := libinit_msm
 TARGET_LIBINIT_DEFINES_FILE := device/samsung/trlteusc/init/init_trlte.cpp
 TARGET_UNIFIED_DEVICE := true
 
-# Radio
-BOARD_RIL_CLASS := ../../../device/samsung/trlteusc/ril
-
 # inherit from the proprietary version
 -include vendor/samsung/trlteusc/BoardConfigVendor.mk
